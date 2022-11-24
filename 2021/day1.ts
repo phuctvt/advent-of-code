6385b85b146101e9cf4d8b158ac2eb4d53e5d6d7
prv <phuc.tran@elca.vn>
2022-11-24 11:13:21 +0700

    day 1 2021

---
+++ 2021/day1.ts
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import path from 'path';
+
+export function getResult(): string {
+    const input = fs.readFileSync(path.join(require.main!.path, 'input.txt'), 'utf-8');
+
+    const measurements = input.split('\n').map(x => +x);
+
+    return `${getPart1(measurements)} ${getPart2(measurements)}`;
+}
+
+function getPart1(measurements: number[]): number {
+    let count = 0;
+    for (let i = 1; i < measurements.length; i++) {
+        const current = measurements[i];
+        const previous = measurements[i - 1];
+        if (current > previous) {
+            count++;
+        }
+    }
+    return count;
+}
+
+function getPart2(measurements: number[]): number {
+    let count = 0;
+    for (let i = 3; i < measurements.length; i++) {
+        const current = measurements[i] + measurements[i - 1] + measurements[i - 2];
+        const previous = measurements[i - 1] + measurements[i - 2] + measurements[i - 3];
+        if (current > previous) {
+            count++;
+        }
+    }
+    return count;
+}
