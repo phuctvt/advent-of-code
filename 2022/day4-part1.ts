3e0ecc34d67aa3aa50730b8acb5ae827a79acbc0
Phuc <phuctv95@gmail.com>
2022-12-05 22:58:44 +0700

    add 2022: day 4, 5

---
+++ 2022/day4-part1.ts
@@ -0,0 +1,18 @@
+import { getAocInput } from '../common/aoc-helper';
+
+export function getResult(): number {
+    const lines = getAocInput().trim().split(/\r\n|\n/);
+    let sum = 0;
+    for (const line of lines) {
+        const [assignment1, assignment2] = line.split(',');
+        const [sectionBegin1, sectionEnd1] = assignment1.split('-').map(x => +x);
+        const [sectionBegin2, sectionEnd2] = assignment2.split('-').map(x => +x);
+        if (
+            sectionBegin1 <= sectionBegin2 && sectionEnd2 <= sectionEnd1 ||
+            sectionBegin2 <= sectionBegin1 && sectionEnd1 <= sectionEnd2
+        ) {
+            sum++;
+        }
+    }
+    return sum;
+}
---
+++ 2022/day5-part2.ts
@@ -0,0 +1,98 @@
+import { getAocInput } from '../common/aoc-helper';
+
+class Crate {
+    value: string = '';
+}
+
+class Stack {
+    id = 0;
+    private crates: Crate[] = [];
+
+    get count() { return this.crates.length; }
+
+    constructor(id: number) {
+        this.id = id;
+    }
+
+    push(crate: Crate) {
+        this.crates.push(crate);
+    }
+
+    /**
+     * First item will be pushed in first and so on.
+     */
+    pushMultiple(crates: Crate[]) {
+        this.crates.push(...crates);
+    }
+
+    pop(size: number = 1): Crate[] {
+        const result = this.crates.slice(this.count - size);
+        this.crates.splice(this.count - size, size);
+        return result;
+    }
+
+    getTop(): Crate {
+        return this.crates[this.count - 1];
+    }
+}
+
+class Step {
+    quantity = 0;
+    from: Stack;
+    to: Stack;
+
+    constructor(quantity: number, from: Stack, to: Stack) {
+        this.quantity = quantity;
+        this.from = from;
+        this.to = to;
+    }
+}
+
+export function getResult(): string {
+    const [stacksRaw, procedureRaw] = getAocInput().trim().split(/\r\n\r\n|\n\n/);
+    const stacks = parseStacks(stacksRaw);
+    const procedure = parseProcedure(procedureRaw, stacks);
+
+    for (const step of procedure) {
+        const crates = step.from.pop(step.quantity);
+        step.to.pushMultiple(crates);
+    }
+
+    return stacks.map(x => x.getTop().value).join('');
+}
+
+function parseStacks(stacksRaw: string): Stack[] {
+    const lines = stacksRaw.split(/\r\n|\n/);
+    const stacks = lines[lines.length - 1].split(' ').filter(x => !!x).map(x => new Stack(+x));
+
+    for (let i = lines.length - 2; i >= 0; i--) {
+        const line = lines[i];
+        const crates = line.match(/.{3,4}/g)!;
+        for (let j = 0; j < stacks.length; j++) {
+            const crate = crates[j].trim().substring(1,2);
+            if (crate) {
+                stacks[j].push({value: crate});
+            }
+        }
+    }
+
+    return stacks;
+}
+
+function parseProcedure(procedureRaw: string, stacks: Stack[]): Step[] {
+    const lines = procedureRaw.split(/\r\n|\n/);
+    const steps: Step[] = [];
+
+    for (const line of lines) {
+        const [quantity, fromId, toId] = line.match(/\d+/g)!.map(x => +x);
+        const from = findStack(stacks, fromId)!;
+        const to = findStack(stacks, toId)!;
+        steps.push(new Step(quantity, from, to));
+    }
+
+    return steps;
+}
+
+function findStack(stacks: Stack[], id: number) {
+    return stacks.find(x => x.id === id);
+}
